f605f45b0a6914166a2a05379421835d713b88c2
Gabriel-Villarinho <160971416+Gabriel-Villarinho@users.noreply.github.com>
2024-09-19 11:22:34 -0300

    Update movimentacao.cs

--- movimentacao.cs
+++ movimentacao.cs
@@ -15,27 +15,27 @@ public class movimentacao : MonoBehaviour
     {
         Time.timeScale = 1f;  // Definindo o timescale para 1
         movimento = new Vector3(-496f, 3.5f, 0f); // definindo valores do vector 3
-        transform.position = movimento; // definindo a posiÁ„o de acordo com vector 3
+        transform.position = movimento; // definindo a posi√ß√£o de acordo com vector 3
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.timeScale == 1f) // Apenas funcionara se o timeScale do jogo for 1, ou seja, quando o jogo estiver rodando
+        if (Time.timeScale == 1f) // Apenas funcionar√° se o timeScale do jogo for 1, ou seja, quando o jogo estiver rodando
         {
-            if (transform.position.x < -201f) //faz com que o personagem ande atÈ chegar no limite
+            if (transform.position.x < -201f) //faz com que o personagem ande at√© chegar no limite
             {
                 movimento.x = 0.05f;
                 movimento.y = 0f;
                 movimento.z = 0f;
                 transform.Translate(movimento);
             }
-            if (transform.position.x > -201f) // Quando chegar no limite muda a cena
+            if (transform.position.x > -201f) // Quando chegar no limite mudar√° a cena
             {
                 SceneManager.LoadScene(nomeDoLevelDejogo);
             }
 
-            if (Input.GetKey(KeyCode.Space)) // fazer o personagem pular
+            if (Input.GetKey(KeyCode.Space)) // ir√° fazer o personagem pular
             {
                 if (transform.position.y < 5f)
                 {
@@ -43,16 +43,16 @@ public class movimentacao : MonoBehaviour
                     movimento.y = 0.03f;
                     movimento.z = 0f;
                     transform.Translate(movimento);
-                    anim.SetBool("Pulo", true); // mudar a animaÁ„o quando pular
-                    anim2.SetBool("Pulo", true); // mudar a animaÁ„o quando pular
+                    anim.SetBool("Pulo", true); // mudar√° a anima√ß√£o quando pular
+                    anim2.SetBool("Pulo", true); // mudar√° a anima√ß√£o quando pular
                 }
             }
             if (Input.GetKeyUp(KeyCode.Space))
             {
-                anim.SetBool("Pulo", false); // mudar a animaÁ„o quando deixar de pular
-                anim2.SetBool("Pulo", false); // mudar a animaÁ„o quando deixar de pular
+                anim.SetBool("Pulo", false); // mudar√° a anima√ß√£o quando deixar de pular
+                anim2.SetBool("Pulo", false); // mudar√° a anima√ß√£o quando deixar de pular
             }
-            else // quando timescale for 0, pausar o personagem
+            else // quando timescale for 0, pausar√° o personagem
             {
                 movimento.x = 0;
                 movimento.y = 0;
