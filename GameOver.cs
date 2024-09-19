c80e52f4162b9c6d8ba34de5cc94fdabf634b0bd
Gabriel-Villarinho <160971416+Gabriel-Villarinho@users.noreply.github.com>
2024-09-19 11:18:03 -0300

    Update GameOver.cs

--- GameOver.cs
+++ GameOver.cs
@@ -18,14 +18,14 @@ public class GameOver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y < 0f) //quando o jogador estiver abaixo do limite, aparecer menu  game over e indicar que o jogador perdeu
+        if (transform.position.y < 0f) //quando o jogador estiver abaixo do limite, aparecerá menu game over e indicar que o jogador perdeu
         {
                 gameOverMenu.gameObject.SetActive(true);
                 Time.timeScale = 0f;
         }
 
     }
-    public void VoltarMenu() // quando apertado, voltar ao menu principal
+    public void VoltarMenu() // quando apertado, voltará ao menu principal
     {
         SceneManager.LoadScene("Menu");
     }
@@ -34,7 +34,7 @@ public class GameOver : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision) // chama a classe collision
     {
-        switch (collision.gameObject.tag) //faz que s� ative quando o jogador encostar no collider com essa tega, ativa o menu gameover
+        switch (collision.gameObject.tag) //faz que só ative quando o jogador encostar no collider com essa tag, ativa o menu gameover
         {
             case "Barreira":
                 gameOverMenu.gameObject.SetActive(true);
